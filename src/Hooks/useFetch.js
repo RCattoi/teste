70793adf0e7694954ee53f2ed2a74efbd526ee17
João Ougano <pedroougano@gmail.com>
2023-04-27 00:47:03 -0300

    comentarios sobre o useFetch

--- src/Hooks/useFetch.js
+++ src/Hooks/useFetch.js
@@ -1,26 +1,41 @@
-import React from 'react';
+import React, { useState, useCallback } from "react";
 
 const useFetch = () => {
-  const [data, setData] = React.useState(null);
-  const [error, setError] = React.useState(null);
-  const [loading, setLoading] = React.useState(false);
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-  const request = React.useCallback(async (url, options) => {
-    let response;
-    let json;
+  /**
+   * Não precisa usar let se você não precisa reatribuir o valor da variavel
+   * Nesse caso, pode declarar direto, usando const
+   */
+  const request = useCallback(async (url, options) => {
+    let json 
     try {
-      setError(null);
+      // não precisa definir o estado pra null se ele já é iniciado com o valor de null
+      // setError(null);
       setLoading(true);
-      response = await fetch(url, options);
+      const response = await fetch(url, options);
       json = await response.json();
-      if (response.ok === false) throw new Error(json.message);
+      /**
+       * Se der algum erro na requisição (vide status diferente da familia do 200) 
+       * ele ja vai estourar no catch direto. Não precisa tratar isso no try
+       * O try é só pra parte de sucesso da requisição
+       */
+      // if (response.ok === false) throw new Error(json.message);
     } catch (err) {
-      json = null;
+      // Não precisa definir o json como null se você nao vai usar ele depois.
+      // json = null;
       setError(err.message);
+      throw new Error(err.message);
     } finally {
       setData(json);
       setLoading(false);
-      return { response, json };
+      /**
+       * Precisa desse retorno? 
+       * Não entendi o intuito dele se o dado retornado ta sendo só o do estado
+       */
+      return { json };
     }
   }, []);
 
