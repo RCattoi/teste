57c8d7b7e4e810ab666ca48cb9a31646135c6b4f
João Ougano <pedroougano@gmail.com>
2023-04-27 01:54:51 -0300

    refactor: ajusta importe de arquivos

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import Background from './Background.js';
-import Search from './Search.js';
+import Background from './components/Background';
+import Search from '../src/components/Search';
 import globalStyle from './style/globalStyle.css';
 
 function App() {
